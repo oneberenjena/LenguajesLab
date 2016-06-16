1a4d8fc358f75a6444013aaa04c1a3e504176a1b
dougtc1 <douglastorres.usb@gmail.com>
2016-06-15 21:16:31 -0400

    bienEtiquetado casi completa, falta verificacion de N y lista completa

--- proyecto.pl
+++ proyecto.pl
@@ -48,57 +48,77 @@ bienEtiquetado(nodo(X,[arista(Y,nodo(Z,[]))])) :- !,writeln('Caso base Num 2 bie
 
 bienEtiquetado(nodo(X,Lista)) :- writeln('Caso recursivo bienEtiquetado'),
 									!, Lnodos = [], Laristas = [],
-									bienEtiquetado_Aristas(Lista,Laristas,Lnodos,X),
-									writeln('Lista Final de Nodos'),writeln(Lnodos), 
-									writeln('Lista Final de Aristas'),writeln(Laristas).
+									bienEtiquetado_Nodos(nodo(X,Lista),Laristas,Lnodos,LaristasF,LnodosF),
+									writeln('Lista Final de Nodos'),writeln(LnodosF), 
+									writeln('Lista Final de Aristas'),writeln(LaristasF).
 											%bienEtiquetado_Nodos(X,Z,lnodos,laristas,Lista).
 % A LOS PREICADOS AUXILIARES QUE ESTAN ABAJO HAY QUE PASARLES NODO Y ARISTA COMO A LOS NORMALES
 % JUNTO CON LAS LISTAS PARA QUE SIRVA
-bienEtiquetado_Nodos([],Lnodos1,Lnodos1).
+bienEtiquetado_Nodos([],Lnodos,Lnodos).
 
-bienEtiquetado_Nodos(nodo(X,[]),Lnodos,Lnodos1) :- writeln('Caso base bienEtiquetado_Nodos'),
+bienEtiquetado_Nodos(nodo(X,[]),Laristas,Lnodos,LaristasR,LnodosR) :- writeln('Caso base bienEtiquetado_Nodos'),
 													writeln(Lnodos),
 													!,not(member(X,Lnodos)),
-													append([X],Lnodos,Lnodos2),
+													append([X],Lnodos,Lnodos1),
 													writeln('Esta es Lnodos'),
-													writeln(Lnodos2),
-													bienEtiquetado_Nodos([],Lnodos2,Lnodos1).
+													writeln(Lnodos1),
+													writeln(Laristas),
+													bienEtiquetado_Aristas([],Laristas,LaristasR),
+													bienEtiquetado_Nodos([],Lnodos1,LnodosR),
+													writeln('ULTIMA LINEA DE bienEtiquetado_Nodos/5 '),
+													writeln(LaristasR),
+													writeln(LnodosR).
 
-bienEtiquetado_Nodos(nodo(X,[Head|Tail]),Lnodos,Laristas) :- writeln('Caso recursivo bienEtiquetado_Nodos'),
+bienEtiquetado_Nodos(nodo(X,[Head|Tail]),Laristas,Lnodos,LaristasR,LnodosR) :- writeln('Caso recursivo bienEtiquetado_Nodos'),
 															!,integer(X)>0,not(member(X,Lnodos)),
 															append([X],Lnodos,Lnodos1),
 															writeln('VOY A LLAMAR A ARISTA HEAD Y ARISTA TAIL'),
-															bienEtiquetado_Aristas(Head,Laristas,Lnodos1,X),
-															bienEtiquetado_Aristas(Tail,Laristas,Lnodos1,X),
-															writeln('Aristas'),
-															writeln(Laristas),
-															writeln(Lnodos1).
+															bienEtiquetado_Aristas(Head,Laristas,Lnodos1,X,Laristas1,Lnodos2),
+															bienEtiquetado_Aristas(Tail,Laristas1,Lnodos2,X,LaristasR,LnodosR),
+															writeln('VOY A VER MIS RESULTADOS'),
+															writeln(LaristasR),
+															writeln(LnodosR).
+															%bienEtiquetado_Aristas([],LaristasR,LaristasQ),
+															%bienEtiquetado_Nodos([],LnodosR,LnodosQ),
+															%writeln('Aristas'),
+															%writeln(LaristasQ),
+															%writeln('Nodos'),
+															%writeln(LnodosQ).
 
-%bienEtiquetado_Nodos([Head|Tail],Laristas,Lnodos,W) :- writeln('Caso recursivo auxiliar bienEtiquetado_Aristas '), 
-%														bienEtiquetado_Aristas(Head,Laristas,Lnodos,W),
-%														bienEtiquetado_Aristas(Tail,Laristas,Lnodos,W).
+%ESTABA MODFICANDO bienEtiquetado PARA QUE LLAME a bienEtiquetado_Nodos Y DE ALLI PASAR A ARISTA, ME FALTA ES VERIFICAR QUE NO TENGA QUE CAMBIAR MAS NADA
+
+bienEtiquetado_Aristas([],Laristas,Laristas).
+
+bienEtiquetado_Aristas([],Laristas,Lnodos,X,LaristasF,LnodosF) :- bienEtiquetado_Aristas([],Laristas,LaristasF),
+																	bienEtiquetado_Nodos([],Lnodos,LnodosF).
 
 
-bienEtiquetado_Aristas([],Laristas,Laristas,X).
-
-bienEtiquetado_Aristas(arista(X,nodo(Y,Hijos)),Laristas,Lnodos,W) :- !, writeln('Caso base recursivo bienEtiquetado_Aristas '), 
+bienEtiquetado_Aristas(arista(X,nodo(Y,Hijos)),Laristas,Lnodos,W,LaristasR,LnodosR) :- !, writeln('Caso base recursivo bienEtiquetado_Aristas '), 
 											not(W = Y),
 											P is abs(W-Y),
 											not(X =:= P) -> (writeln('Arbol no esta bien etiquetado: Etiqueta de arista es distinta a diferencia entre padre e hijo.'), false);
 											P is abs(W-Y),
 											writeln('Etiqueta padre: W - Etiqueta arista: X - Etiqueta hijo: Y - Diferencia: P - Lnodos - Laristas'),
 											write(W),write(' '), write(X),write(' '), write(Y),write(' '), 
-											write(P),write(' '), write(Lnodos),write(' '), write(Laristas),
+											write(P),write(' '), write(Lnodos),write(' '), write(Laristas),nl,
 											not(member(X,Laristas)),
-											not(member(W,Lnodos)),
-											append([W],Lnodos,Lnodos1), writeln(Lnodos1),
-											append([X],Laristas,Laristas1), writeln(Laristas1), 
-											bienEtiquetado_Nodos(nodo(Y,Hijos),Lnodos1,Lnodos2).
+											append([X],Laristas,Laristas1), writeln(Laristas1),writeln(Lnodos), writeln(Hijos),
+											bienEtiquetado_Nodos(nodo(Y,Hijos),Laristas1,Lnodos,LaristasR,LnodosR),
+											writeln('ULTIMA LINEA DE bienEtiquetado_Aristas/6-1 '),
+											writeln(LaristasR),
+											writeln(LnodosR).
 											%bienEtiquetado_Aristas([],Laristas,Laristas).
 
-bienEtiquetado_Aristas([Head|Tail],Laristas,Lnodos,W) :- writeln('Caso recursivo auxiliar bienEtiquetado_Aristas '), 
-														bienEtiquetado_Aristas(Head,Laristas,Lnodos,W),
-														bienEtiquetado_Aristas(Tail,Laristas,Lnodos,W).
+bienEtiquetado_Aristas([Head|Tail],Laristas,Lnodos,W,LaristasF,LnodosF) :- writeln('Caso recursivo auxiliar bienEtiquetado_Aristas'),
+														bienEtiquetado_Aristas(Head,Laristas,Lnodos,W,Laristas2,Lnodos2),
+														writeln('Falle'), writeln(Tail),
+														writeln(Laristas2), writeln(Lnodos2), writeln(W),
+														bienEtiquetado_Aristas(Tail,Laristas2,Lnodos2,W,LaristasF,LnodosF),
+														writeln('ULTIMA LINEA DE bienEtiquetado_Aristas/6-2 '),
+														writeln(LaristasF),
+														writeln(LnodosF).
+														%bienEtiquetado_Aristas([],Laristas,LaristasR),
+														%bienEtiquetado_Nodos([],Lnodos1,LnodosR).
 
 
 % bienEtiquetado(nodo(V,[arista(E,nodo(W,[]))])) :- append([W], ListaNodos, ListaNodos)
